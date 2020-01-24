7644b3b095537872e714e8de9165b2e8f2090c5a
rahul <rahulsirohi14321@gmail.com>
2020-01-24 16:45:53 +0530

    changes made by US Architect

--- src/com/automation/ExecutionEngine/Forgit.java
+++ src/com/automation/ExecutionEngine/Forgit.java
@@ -15,5 +15,11 @@ public class Forgit {
 		System.out.println("New Change for git test by X Person from Asia");
 		System.out.println("One more New Change for git test by X Person from Asia");
 	}
+	public void POM()
+	{
+		System.out.println("Change by architect in US");
+		System.out.println("New Change by architect for POM in US");
+		System.out.println("One more New Change by US");
+	}
 
 }
